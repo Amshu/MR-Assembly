5899fd24423762ec750bfaf0fc71a74a170cfd6d
DML_PC <50893358+CUBE-Deakin-University@users.noreply.github.com>
2021-04-29 15:18:46 +1000

    ver 0.0.6 - Added step buttons and workbench

---
+++ HYDAC_MR/Assets/HYDAC_EView/Scripts/ENUMS.cs
@@ -0,0 +1,6 @@
+﻿public enum EACTIONS
+{
+    IMPLODE,
+    EXPLODE,
+    HIGHLIGHT
+}
---
+++ HYDAC_MR/Assets/HYDAC_EView/Scripts/MPart/IMachinePart.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace HYDAC_EView.Scripts.MPart
+{
+    public interface IMachinePart
+    {
+        void Initialize();
+        int GetAssemblyPosition();
+        string GetPartName();
+        void Implode(float timeToDest);
+        void Explode(float timeToDest);
+        void HighlightPart(bool toggle, Material highlightMaterial);
+    }
+}
