4dc2384799c6410a381bb0bc6d867774dd5d1575
Amshu <amshumaanrakshith@gmail.com>
2021-06-12 19:04:57 +1000

    ver 0.2.0 - Added HyboxMaxi Model and Scene

---
+++ PiXYZ/GeometricOptimization.py
@@ -0,0 +1,19 @@
+modelID = scene.getSelectedOccurrences()
+
+# Repair CAD
+algo.repairCAD(modelID, 0.100000, True)
+
+# Tesselate - First three params (0.5, 100, -1)
+algo.tessellate([1], 0.500000, -1, -1, True, 2, 1, 0.000000, False, False, True, False)
+
+# Repair Mesh
+algo.repairMesh(modelID, 0.100000, True, True)
+
+# Smart Orient
+algo.smartOrient(modelID, 500.000000, 1.000000, 512, 1, False, 1)
+
+# Hidden Removal
+_ret_ = algo.hiddenRemoval(modelID, 0, 4096, 16, 90.000000, False, 1)
+
+# Smart Hidden Removal
+_ret_ = algo.smartHiddenRemoval(modelID, 2, 500.000000, 1.000000, 1024, 1, False, 1)
---
+++ PiXYZ/TextureBaking.py
@@ -0,0 +1,5 @@
+modelID = scene.getSelectedOccurrences()
+
+_ret_ = algo.bakeMaps(modelID, [], [0, 1, 5, 3, 6], 0, 2048, 1, True, "", [], -1, 0, 0.100000)
+
+scene.removeMaterials()
