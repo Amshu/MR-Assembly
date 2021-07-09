d13fc3e5bbb955424a2c5d2a5618bc7feea401c9
Amshu <amshumaanrakshith@gmail.com>
2021-07-09 14:44:10 +1000

    Fixed the tooltips to follow on pickup

--- HYDAC_MR/Assets/HYDAC/Scripts/NET/NetManager.cs
+++ HYDAC_MR/Assets/HYDAC/Scripts/NET/NetManager.cs
@@ -1,17 +1,26 @@
+using System.Numerics;
 using Normal.Realtime;
 using UnityEngine;
+using UnityEngine.Serialization;
+using Vector3 = UnityEngine.Vector3;
 
 namespace HYDAC.Scripts.NET
 {
     public class NetManager : MonoBehaviour
     {
-        [SerializeField] private Realtime normRealtime;
+        [SerializeField] private Realtime _normRealtime = null;
+        
+        [SerializeField] private Transform _spawnPoint = null;
+        
+        private RealtimeAvatarManager _normAvatarManager = null;
 
-        private RealtimeAvatarManager _normAvatarManager = null; 
-    
+        private int _remoteUsersInRoom = 0;
+        
+        public int UsersInRoom => _remoteUsersInRoom + 1;
+
         private void Awake()
         {
-            normRealtime.didConnectToRoom += OnTryToConnect;
+            _normRealtime.didConnectToRoom += OnTryToConnect;
         }
 
         private void OnTryToConnect(Realtime realtime)
@@ -34,11 +43,23 @@ namespace HYDAC.Scripts.NET
             if (islocalavatar)
             {
                 Debug.Log("#NetManager#-------------------------OnAvatarCreated: Local avatar created");
+                Debug.Log("#NetManager#-------------------------No of users in room: " + UsersInRoom);
+
+                if(UsersInRoom > 1)
+                    AdjustPosition(UsersInRoom);
             }
             else
             {
                 Debug.Log("#NetManager#-------------------------OnAvatarCreated: Remote created");
-            
+                _remoteUsersInRoom++;
+            }
+        }
+
+        private void AdjustPosition(int noOfUsers)
+        {
+            int offset = 1;
+            for (int i = 0; i < UsersInRoom; i++)
+            {
             }
         }
     }
